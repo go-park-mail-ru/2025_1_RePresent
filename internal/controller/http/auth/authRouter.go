8db736b9ac10be00c098630052db825443ed64fe
WonderBro <froloff1830@gmail.com>
2025-03-08 02:52:18 +0300

    RE:21 Добавлена предварительная проверка авторизованности пользователя перед запросом
    Нужно будет пересмотреть предварительную авторизацию

--- internal/controller/http/auth/authRouter.go
+++ internal/controller/http/auth/authRouter.go
@@ -1,9 +1,9 @@
 package auth
 
 import (
-	"net/http"
-
+	"RE/internal/controller/http/middleware"
 	"RE/internal/usecase/auth"
+	"net/http"
 )
 
 type AuthController struct {
@@ -18,10 +18,11 @@ func SetupAuthRoutes(usecase *auth.AuthUsecase) http.Handler {
 	mux := http.NewServeMux()
 	authController := NewAuthController(usecase)
 
+	mux.Handle("/auth/me", middleware.AuthMiddleware(usecase)(http.HandlerFunc(authController.getCurrentUserHandler)))
+	mux.Handle("/auth/logout", middleware.AuthMiddleware(usecase)(http.HandlerFunc(authController.LogoutHandler)))
+
 	mux.HandleFunc("/auth/login", authController.LoginHandler)
 	mux.HandleFunc("/auth/signup", authController.RegisterHandler)
-	mux.HandleFunc("/auth/logout", authController.LogoutHandler)
-	mux.HandleFunc("/auth/me", authController.getCurrentUserHandler)
 
 	return mux
 }
---
+++ internal/controller/http/middleware/checkAuthorize.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"RE/internal/usecase/auth"
+	"net/http"
+)
+
+func AuthMiddleware(usecase *auth.AuthUsecase) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie("session_id")
+			if err != nil || cookie.Value == "" {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			_, err = usecase.GetUserBySessionID(cookie.Value)
+			if err != nil {
+				http.Error(w, "Unauthorized: User not found", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
