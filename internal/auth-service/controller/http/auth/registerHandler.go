fc6161b3d5979dd6831bbd71b64e4d4d4d66e55e
sh1sh4 <sh1sh4@vk.com>
2025-04-04 04:46:56 +0300

    RE-36: disabled mail check

--- internal/auth-service/controller/http/auth/registerHandler.go
+++ internal/auth-service/controller/http/auth/registerHandler.go
@@ -10,7 +10,7 @@ import (
 type RegisterRequest struct {
 	Username string `json:"username" validate:"required,min=3,max=20"`
 	Email    string `json:"email" validate:"email,required"`
-	Code     string `json:"code" validate:"required,len=6"`
+	// Code     string `json:"code" validate:"required,len=6"`
 	Password string `json:"password" validate:"required,min=8"`
 	Role     int    `json:"role" validate:"required,gte=1,lte=2"`
 }
