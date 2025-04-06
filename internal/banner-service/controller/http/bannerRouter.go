4196b9138a3784256aa6cbfe57126f00cdc826eb
sh1sh4 <sh1sh4@vk.com>
2025-04-06 03:41:47 +0300

    RE-39: FIX bannerRouter

--- internal/banner-service/controller/http/bannerRouter.go
+++ internal/banner-service/controller/http/bannerRouter.go
@@ -22,10 +22,10 @@ func SetupBannerRoutes(authenticator *authenticate.Authenticator, bannerUsecase 
 	bannerController := NewBannerController(bannerUsecase, imageUsecase)
 	// middleware.AuthMiddleware(authUsecase)()
 	muxRouter.Handle("/api/v1/banner/", authenticate.AuthMiddleware(authenticator)(http.HandlerFunc(bannerController.GetUserBanners)))
-	muxRouter.Handle("/api/v1/banner/{banner_id}", authenticate.AuthMiddleware(authenticator)(http.HandlerFunc(bannerController.BannerHandleFunc)))
+	muxRouter.Handle("/api/v1/banner/{banner_id}", (http.HandlerFunc(bannerController.BannerHandleFunc)))
 	muxRouter.Handle("/api/v1/banner/create", authenticate.AuthMiddleware(authenticator)(http.HandlerFunc(bannerController.CreateBanner)))
-	muxRouter.Handle("/api/v1/banner/upload", authenticate.AuthMiddleware(authenticator)(http.HandlerFunc(bannerController.UploadImageHandler)))
-	muxRouter.Handle("/api/v1/banner/image/{image_id}", authenticate.AuthMiddleware(authenticator)(http.HandlerFunc(bannerController.DownloadImage)))
+	muxRouter.Handle("/api/v1/banner/upload", (http.HandlerFunc(bannerController.UploadImageHandler)))
+	muxRouter.Handle("/api/v1/banner/image/{image_id}", (http.HandlerFunc(bannerController.DownloadImage)))
 
 	return muxRouter
 }
