6de907ff2163f0ff6710205984e735d1648d8c3d
sh1sh4 <sh1sh4@vk.com>
2025-04-09 14:31:21 +0300

    RE-55:

--- cmd/banner-service/main.go
+++ cmd/banner-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,6 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cfg)
 	go app.Run(cfg)
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
