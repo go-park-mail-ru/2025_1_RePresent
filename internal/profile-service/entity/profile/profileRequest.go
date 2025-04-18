552bab10fa77c7b57c7ef78c60155d45c98efb79
sh1sh4 <sh1sh4@vk.com>
2025-04-18 23:52:41 +0300

    RE-59: The Night Before

--- internal/profile-service/entity/profile/profileRequest.go
+++ internal/profile-service/entity/profile/profileRequest.go
@@ -2,5 +2,5 @@ package profile
 
 type ProfileRequest struct {
 	Username    string `json:"username" validate:"required,min=5,max=35"`
-	Description string `json:"description" validate:"required,min=0,max=200"`
+	Description string `json:"description" validate:"max=200"`
 }
