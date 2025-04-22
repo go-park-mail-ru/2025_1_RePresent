92b11be408e08668052391cfccd5d6da35fa7b68
WonderBro <froloff1830@gmail.com>
2025-04-22 17:52:13 +0300

    RE-XX: fix username length, refactor banner service

--- internal/profile-service/entity/profile/profileRequest.go
+++ internal/profile-service/entity/profile/profileRequest.go
@@ -1,6 +1,6 @@
 package profile
 
 type ProfileRequest struct {
-	Username    string `json:"username" validate:"required,min=5,max=35"`
+	Username    string `json:"username" validate:"required,min=5,max=50"`
 	Description string `json:"description" validate:"max=200"`
 }
--- internal/profile-service/entity/profile/profileResponse.go
+++ internal/profile-service/entity/profile/profileResponse.go
@@ -2,7 +2,7 @@ package profile
 
 type ProfileResponse struct {
 	ID          int    `json: "id"`
-	Username    string `json:"username" validate:"required,min=5,max=35"`
+	Username    string `json:"username" validate:"required,min=5,max=50"`
 	Email       string `json:"email" validate:"required,email"`
 	Description string `json:"description" validate:"min=0,max=200"`
 	Balance     int    `json:"balance" validate:"gte=0"`
