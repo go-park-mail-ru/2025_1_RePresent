67bfc530930c9dabd77fceb84421353ef7140ad9
KiberPerdun <Suisuikin@yandex.ru>
2025-04-26 12:01:07 +0300

    RE-75: clickhouse scheme

---
+++ deploy/clickhouse/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE reviews (
+    id UUID DEFAULT generateUUIDv4(),
+    question_id UUID,
+    page text,
+    comment VARCHAR(200),
+    rating INT,
+    created_at TIMESTAMP DEFAULT toTimeZone(now(), 'Europe/Moscow')
+) ENGINE = MergeTree()
+ORDER BY (created_at);
+
+CREATE TABLE questions (
+    id UUID DEFAULT generateUUIDv4(),
+    question text
+) ENGINE = MergeTree()
+ORDER BY (id);
