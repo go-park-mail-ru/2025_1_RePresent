9a34dc5140c45fb5e5dfcdbca2cd3e23d643c79f
sh1sh4 <sh1sh4@vk.com>
2025-04-26 13:09:27 +0300

    RE-75: ShowQuestionByPageID Handler + usecase ready

--- internal/csat-service/controller/http/csat/showQuestionByPageID.go
+++ internal/csat-service/controller/http/csat/showQuestionByPageID.go
@@ -1,6 +1,29 @@
 package csat
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	// "retarget/csat-service/entity"
+	response "retarget/pkg/entity"
+	// "strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func (c *CsatController) ShowQuestionByPageID(w http.ResponseWriter, r *http.Request) {
+	_, ok := r.Context().Value(response.UserContextKey).(response.UserContext)
+	if !ok {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response.NewResponse(true, "Error of authenticator"))
+	}
+	vars := mux.Vars(r)
+	pageID := vars["page_id"]
+	question, err := c.csatUsecase.GetQuestionByPage(pageID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(response.NewResponse(true, err.Error()))
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response.NewResponseWithBody(false, "Error of authenticator", question))
 }
