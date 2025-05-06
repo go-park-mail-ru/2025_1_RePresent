cf9624b08d71d0ed93777c46c865eea3405bb985
sh1sh4 <sh1sh4@vk.com>
2025-05-06 15:05:57 +0300

    RE-21: bababoi

--- internal/profile-service/entity/profile/profileResponse.go
+++ internal/profile-service/entity/profile/profileResponse.go
@@ -1,10 +1,12 @@
 package profile
 
+import "gopkg.in/inf.v0"
+
 type ProfileResponse struct {
 	ID          int     `json: "id"`
 	Username    string  `json:"username" validate:"required,min=5,max=50"`
 	Email       string  `json:"email" validate:"required,email"`
 	Description string  `json:"description" validate:"min=0,max=200"`
-	Balance     Decimal `json:"balance" validate:"gte=0"`
+	Balance     inf.Dec `json:"balance" validate:"gte=0"`
 	Role        int     `json:"role" validate:"required,gte=1,lte=2"`
 }
