8e159325b4fe8f7350d6aeaaa1fcc013d4d6449b
sh1sh4 <sh1sh4@vk.com>
2025-05-07 13:26:17 +0300

    RE: fixes

--- internal/banner-service/controller/http/middleware/cors.go
+++ internal/banner-service/controller/http/middleware/cors.go
@@ -6,26 +6,16 @@ import (
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			origin = "*"
-		}
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-
+		w.Header().Set("Access-Control-Allow-Origin", "http://re-target.ru")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
-
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
