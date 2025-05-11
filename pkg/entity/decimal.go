a042d9210480ad9e90983dbff33d137c123da607
sh1sh4 <sh1sh4@vk.com>
2025-05-11 18:58:43 +0000

    RE-79: .gitignore fixes

---
+++ pkg/entity/decimal.go
@@ -0,0 +1,78 @@
+package entity
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"strings"
+
+	"gopkg.in/inf.v0"
+)
+
+type Dec struct {
+	*inf.Dec
+}
+
+func NewDec(s string) (*Dec, error) {
+	d := new(inf.Dec)
+	if _, ok := d.SetString(s); !ok {
+		return nil, fmt.Errorf("invalid decimal format: %s", s)
+	}
+	return &Dec{Dec: d}, nil
+}
+
+func (d *Dec) Scan(value interface{}) error {
+	if value == nil {
+		d.Dec = inf.NewDec(0, 0)
+		return nil
+	}
+
+	var s string
+	switch v := value.(type) {
+	case []byte:
+		s = string(v)
+	case string:
+		s = v
+	default:
+		return fmt.Errorf("unsupported type: %T", value)
+	}
+
+	s = strings.TrimLeft(s, "+")
+
+	var err error
+	d.Dec, err = parseDecimal(s)
+	return err
+}
+
+func parseDecimal(s string) (*inf.Dec, error) {
+	d := new(inf.Dec)
+	if _, ok := d.SetString(s); !ok {
+		return nil, fmt.Errorf("invalid decimal format: %s", s)
+	}
+	return d, nil
+}
+
+func (d Dec) Value() (driver.Value, error) {
+	if d.Dec == nil {
+		return "0", nil
+	}
+	return d.String(), nil
+}
+
+func (d *Dec) UnmarshalJSON(data []byte) error {
+	s := strings.Trim(string(data), `"`)
+	if s == "null" || s == "" {
+		d.Dec = inf.NewDec(0, 0)
+		return nil
+	}
+
+	var err error
+	d.Dec, err = parseDecimal(s)
+	return err
+}
+
+func (d Dec) MarshalJSON() ([]byte, error) {
+	if d.Dec == nil {
+		return []byte(`"0"`), nil
+	}
+	return []byte(`"` + d.String() + `"`), nil
+}
