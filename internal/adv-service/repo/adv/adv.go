353117fcad1f545b3e76717bb6b95655fcf30684
sh1sh4 <sh1sh4@vk.com>
2025-05-26 14:04:46 +0300

    RE-79:

--- internal/adv-service/repo/adv/adv.go
+++ internal/adv-service/repo/adv/adv.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"retarget/internal/adv-service/entity/adv"
+	"retarget/pkg/entity"
 
 	_ "github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/gocql/gocql"
@@ -18,7 +19,7 @@ type AdvRepositoryInterface interface {
 	CreateLink(link adv.Link) error
 	FindLinksByUser(userID int) ([]adv.Link, error)
 	DeleteLink(link string) error
-	WriteMetric(bannerID int, slotLink string, action string) error
+	WriteMetric(bannerID int, slotLink string, action string, price entity.Decimal) error
 	GetSlotMetric(slotID, action string, from, to time.Time) (map[string]int, error)
 	GetSlotCTR(slotID, action string, from, to time.Time) (map[string]float64, error)
 	GetSlotRevenue(slotID, action string, from, to time.Time) (map[string]float64, error)
@@ -105,16 +106,17 @@ func (u *AdvRepository) DeleteLink(link string) error {
 	return nil
 }
 
-func (u *AdvRepository) WriteMetric(bannerID int, slotLink string, action string) error {
+func (u *AdvRepository) WriteMetric(bannerID int, slotLink string, action string, price entity.Decimal) error {
 	const addQuery = `
 		INSERT INTO actions (
 			banner_id, 
 			slot_id, 
-			actions
-		) VALUES (?, ?, ?)
+			actions,
+			price
+		) VALUES (?, ?, ?, ?)
 	`
 
-	res, err := u.clickhouse.Exec(addQuery, bannerID, slotLink, action)
+	res, err := u.clickhouse.Exec(addQuery, bannerID, slotLink, action, price.Dec)
 	if err != nil {
 		log.Printf("ClickHouse insert error: %v", err)
 		return err
