9ec792d476f1dc3a5a208d4ae17cd1d58f05447e
WonderBro <froloff1830@gmail.com>
2025-05-27 22:13:52 +0300

    RE-84: change from async deploy to sync

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -17,11 +17,31 @@ docker builder prune -a -f
 docker system df
 
 # Deploy new services
-docker compose up -d --build kafka scylla
-docker compose up -d --build redis0 postgresdb minio clickhouse
+# Storages
+docker compose up -d --build kafka 
+docker compose up -d --build scylla
+
+docker compose up -d --build postgresdb 
+docker compose up -d --build redis0 
+docker compose up -d --build minio 
+docker compose up -d --build clickhouse
+
+# Start init scripts
 docker exec ReTargetScylla cqlsh -f /scylla_scripts/init.cql
-docker exec -it ReTargetKafka /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic balance_notification_topic --partitions 3 --replication-factor 1
-docker compose up -d --build auth banner profile mail adv csat pay
+docker exec ReTargetKafka /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic balance_notification_topic --partitions 3 --replication-factor 1
+
+# Services
+docker compose up -d --build auth
+docker compose up -d --build banner 
+docker compose up -d --build profile 
+docker compose up -d --build pay
+docker compose up -d --build adv 
+docker compose up -d --build mail 
+
+docker compose up -d --build csat 
+
+# Utils
+docker compose up -d --build grafana
 
 # Web-server reload
 cd ..
