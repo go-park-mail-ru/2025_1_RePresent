0584f7f30a3bb8976b4d196e3769ab5fdb7ad7ea
WonderBro <froloff1830@gmail.com>
2025-05-27 21:41:44 +0300

    RE-84: add full deploy

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,10 +5,10 @@ echo "Working dir: $(pwd)"
 
 ls
 
-# Deploy
 cd backend_2025_1_RePresent/
 sudo git pull
 
+# Clear old services
 cd deploy/
 docker compose down
 docker container prune -f
@@ -16,5 +16,20 @@ docker image prune -a -f
 docker builder prune -a -f
 docker system df
 
-docker compose up -d --build
+# Deploy new services
+docker compose up -d --build kafka scylla
+docker compose up -d --build redis0 postgresdb minio clickhouse
+docker exec ReTargetScylla cqlsh -f /scylla_scripts/init.cql
+docker exec -it ReTargetKafka /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --topic balance_notification_topic --partitions 3 --replication-factor 1
+docker compose up -d --build auth banner profile mail adv csat pay
+
+# Web-server reload
+cd ..
+cd ..
+cd ReTargetResty/
+make hhr
+make hot-reload
+
+# Check result
+docker ps -a
 echo "✅ Deploy successful finished"
